2dc8192c061ba9b53e7e791e5592fc7093c5bdd1
Simona Florusová <florusova.simona@seznam.cz>
2024-08-31 14:57:01 +0200

    My first project

---
+++ js/script.js
@@ -0,0 +1,82 @@
+window.onload = () => {
+    document.querySelectorAll(".resizable").forEach((element) => {
+        element.onclick = () => {
+            element.classList.toggle("bigger");
+        };
+    });
+
+    const up = document.querySelector(".up");
+    up.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+    });
+    const hideUp = () => {
+        if (window.scrollY < 500) {
+            up.classList.add("hidden");
+        } else {
+            up.classList.remove("hidden");
+
+        }
+    };
+    hideUp();
+    window.onscroll = () => {
+        hideUp();
+    };
+
+    $(document).ready(() => {
+        $(".more").each((i, more) => {
+            $(more).click(() => {
+                const show = $(more).attr("data-show");
+                $(show).toggleClass("hidden");
+                $(more).text(
+                    $(show).hasClass("hidden") ? "Zobrazit více" : "Zobrazit méně"
+                );
+            });
+        });
+    });
+};
+
+(function ($) {
+    $(function () {
+        // sem přijde naše jQuery 
+        /* Scroll buttons to sections */
+        $(".jq--scroll-button-first").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--cooperation").offset().top }, 100);
+        });
+
+        $(".jq--scroll-button-second").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--eshop").offset().top }, 300);
+        });
+
+        $(".jq--scroll-button-third").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--products").offset().top }, 300);
+        });
+    });
+
+    /* Mobile navigation */
+    $(".jq--nav-icon").click(function () {
+        $(".mobile-nav-back").slideToggle();
+        $("nav ul").fadeToggle();
+    });
+
+    /* Change hamburger to cross vice versa */
+
+    $(".jq--image-hamburger").click(function () {
+
+        if ($(".jq--image-hamburger").attr("src") == "img/hamburgerMenu.png") {
+            $($(".jq--image-hamburger").attr("src", "img/crossMenu.png"));
+        }
+        else {
+            $($(".jq--image-hamburger").attr("src", "img/hamburgerMenu.png"));
+        }
+    });
+
+})(jQuery);
+
+
+
+
+
